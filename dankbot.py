95defc43898aca6741ccc6f5cd7c8a2bba1deb43
prozn <prozn@quickmind.co.uk>
2016-11-24 08:06:44 +0000

    Consolidated dankbot_run.py and bot.py into dankbot.py
    Consolidated dankbot_run.py and bot.py into dankbot.py in root
    directory - no need to complicate a one file project.

--- dankbot.py
+++ dankbot.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import daemon
+import argparse
+import os
 import time
 import requests
 import configparser
@@ -234,4 +240,25 @@ def sendKill(killtype, searchsection, km):
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--nodaemon", help="Do not run in daemon mode.", action="store_true")
+    parser.add_argument("--config", help="Specify path to folder containing config files.")
+
+    args = parser.parse_args()
+
+    if args.nodaemon:
+        print("Running bot in the terminal.")
+        if args.config:
+            print("Configuration path specified: %s" % os.path.abspath(args.config))
+            main(os.path.abspath(args.config))
+        else:
+            main()
+    else:
+        print("Running bot in daemon mode.")
+        with daemon.DaemonContext():
+            if args.config:
+                print("Configuration path specified: %s" % os.path.abspath(args.config))
+                main(os.path.abspath(args.config))
+            else:
+                main()
